5b21be9b8b2f5a19d32506f06fca3bcba2829a18
pprevos <peter@prevos.net>
2018-04-17 11:15:14 +1000

    Qualitative data science.

---
+++ Hydroinformatics/Macromarketing/interviews.R
@@ -0,0 +1,76 @@
+# INIT
+library(tidyverse)
+library(RQDA)
+library(tm)
+library(wordcloud)
+library(topicmodels)
+library(igraph)
+
+# Word cloud
+openProject("Hydroinformatics/Macromarketing/stakeholders.rqda")
+interviews <- RQDAQuery("SELECT file FROM source")
+interviews$file <- apply(interviews, 1, function(x) gsub("…", "...", x))
+interviews$file <- apply(interviews, 1, function(x) gsub("’", "", x))
+interviews <- Corpus(VectorSource(interviews$file))
+interviews <-  tm_map(interviews, stripWhitespace)
+interviews <-  tm_map(interviews, content_transformer(tolower))
+interviews <-  tm_map(interviews, removeWords, stopwords("english"))
+interviews <-  tm_map(interviews, removePunctuation)
+interviews <-  tm_map(interviews, removeNumbers)
+interviews <-  tm_map(interviews, removeWords, c("interviewer", "interviewee"))
+
+pdf("Hydroinformatics/Macromarketing/wordcloud.pdf")
+set.seed(1969)
+wordcloud(interviews, min.freq = 10, max.words = 50, rot.per=0.35, 
+          colors = brewer.pal(8, "Blues")[-1:-5])
+dev.off()
+
+# Topic Models
+dtm <- DocumentTermMatrix(interviews)
+dtm <- removeSparseTerms(dtm, 0.99)
+ldaOut <-LDA(dtm, k = 4)
+terms(ldaOut,6)
+
+
+
+#Load and transform data
+codings <- getCodingTable()[,4:5]
+categories <- RQDAQuery("SELECT filecat.name AS category, source.name AS filename 
+                         FROM treefile, filecat, source 
+                         WHERE treefile.catid=filecat.catid AND treefile.fid=source.id AND treefile.status=1")
+codings <- merge(codings, categories, all.y = TRUE)
+head(codings)
+
+# Open coding
+reorder_size <- function(x) {
+    factor(x, levels = names(sort(table(x))))
+}
+ggplot(codings, aes(reorder_size(codename), fill=category)) + geom_bar(stat="count") + 
+    facet_grid(~filename) + coord_flip() + 
+    theme(legend.position="bottom", legend.title=element_blank()) + 
+    ylab("Code frequency in interviews") + xlab("Code")
+ggsave("Hydroinformatics/Macromarketing/rqda_codes.png", width=9, height=11, dpi = 300)
+
+
+
+# Axial coding
+edges <- RQDAQuery("SELECT codecat.name, freecode.name FROM codecat, freecode, treecode 
+          WHERE codecat.catid=treecode.catid AND freecode.id=treecode.cid")
+
+g <- graph_from_edgelist(as.matrix(edges), directed = FALSE) %>%
+  simplify()
+V(g)$name <- gsub(" ", "\n", V(g)$name)
+
+c <- spinglass.community(g)
+pdf("Hydroinformatics/Macromarketing/network.pdf")
+par(mar=rep(0,4))
+set.seed(666)
+plot(c, g, 
+     vertex.size = 10,
+     vertex.color = NA,
+     vertex.frame.color = NA,
+     layout = layout.drl)
+dev.off()
+
+
+
